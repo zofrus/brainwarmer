a7d5ec039718338ea41b282c295a4ae839ac46ec
Jason Fuller <jason.fuller@endurance.com>
2018-03-16 13:15:59 -0600

    v.0.1 calculator - Revealing module pattern es5

---
+++ proj1/app.js
@@ -0,0 +1,38 @@
+
+var calc= (function(num1, num2, num3, num4) {
+    'use strict';
+
+   
+    function total() {
+        
+    }
+
+    function add() {
+        
+    }
+   
+    function subtract() {
+        
+    }
+    
+    function divide() {
+        
+    }
+
+    function multiply() {
+        
+    }
+
+    function concat() {
+        
+    }
+
+    return {
+        add:add,
+        subtract:sub,
+        divide:divide,
+        multiply:mult,
+        concat:concat,
+        total:total,
+    };
+}());
